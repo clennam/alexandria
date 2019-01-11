6697e6808f8178de7c2e9e72239aff3da942bb8c
clennam <amkahandawala@gmail.com>
2019-01-12 03:10:21 +0800

    Extensive comments in filter_books.js

--- assets/iphone/js/filter_books.js
+++ assets/iphone/js/filter_books.js
@@ -1,18 +1,28 @@
+// function that controls loading of an array of objects into the booklist listview
 function refreshBookList(books) {
+    // clear book list
     $("#booklist").html('');
+    // if the array passed is not null or empty
     if (books) {
+        // initialise listview (this is important, do this first to avoid lifecycle troubles)
         $("#booklist").listview();
+        //for each element in the array
         books.forEach(function (book) {
+            // (capitalise the genre field so it looks better in the app, not important)
             book.genre = book.genre.charAt(0).toUpperCase() + book.genre.slice(1);
+            // Just insert raw html and add the necessary text in the appropriate places to the booklist listview div to make this
+            // actual html
             $("#booklist").append('<li><a class="booklist-item" href="#book" data-transition="slide"><img src="' + book.image + '" /><h2>' + book.title + '</h2><p>' + book.author + ' | ' + book.genre + '</p><span class="ui-li-count">' + book.price + '</span></a></li>');
         });
+        // refresh listview, this is important as it adds css to our new booklist, do this one before last
         $("#booklist").listview("refresh");
 
-        // apply books list-specific css, requires corresponding markup in html
+        // apply books list-specific css that has not already been added. Do this last, and after you refresh the listview css.
         $('.booklist-item > h2, p').css('width', '82%');
     }
 }
 
+// function that controls how books are reordered according to criteria
 function sortBooks(books) {
     return jQuery.grep(books, function (book) {
         if ($("#genre").val() != 'all-genres') {
@@ -55,7 +65,7 @@ $(function () {
         }
     ];
 
-    // Generate the book list items from the array and add them to the page
+    // Generate the book list items from the array, sort them in initial order, and add them to the page
     books.sort(function (a, b) {
         if (a.title < b.title) { return -1; }
         if (a.title > b.title) { return 1; }
@@ -63,6 +73,7 @@ $(function () {
     });
     refreshBookList(books);
 
+    // Reorder books when filters change
     $("#genre").change(function () {
         var sortedBooks = sortBooks(books);
         refreshBookList(sortedBooks);
