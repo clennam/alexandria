cf89d772483a843b7e3436e6fe9f57c015af69c6
Thushini Ranasinghe <thushini.2014013@iit.ac.lk>
2019-01-14 11:12:17 +0530

    Add files via upload
    Change header

--- assets/ipad/js/load_panels.js
+++ assets/ipad/js/load_panels.js
@@ -10,9 +10,9 @@ $(function () {
 			var sessionEmail = sessionStorage.getItem('email');
 			var sessionTokens = sessionStorage.getItem('tokens');
 			if (sessionFirstName && sessionLastName && sessionEmail && sessionTokens){
-				$("#userStatus").replaceWith("<div class='ui-grid-a' id='userStatus'><div class='ui-block-a'><a href='#menu' data-role='button' data-rel='popup'>Hi Thushini<span class='ui-nodisc-icon ui-icon-carat-d ui-btn-icon-right'></span></a></div></div>");
+				$("#userStatus").replaceWith("<div class='ui-grid-a' id='userStatus'><div class='ui-block-a'><a href='#menu' data-role='button' data-rel='popup' class='homeBtn'>Hi "+ sessionFirstName +"<span class='ui-nodisc-icon ui-icon-carat-d ui-btn-icon-right'></span></a></div></div>");
 			} else {
-				$("#userStatus").replaceWith("<div class='ui-grid-a' id='userStatus'><div class='ui-block-a'><a href='signin.html' data-ajax='false' class='ui-btn ui-corner-all'>Sign In</a></div><div class='ui-block-b'><a href='signup.html' data-ajax='false' class='ui-btn ui-corner-all'>Sign Up</a></div></div>");
+				$("#userStatus").replaceWith("<div class='ui-grid-a' id='userStatus'><div class='ui-block-a'><a href='signin.html' data-ajax='false' class='ui-btn ui-corner-all homeBtn'>Sign In</a></div><div class='ui-block-b'><a href='signup.html' data-ajax='false' class='ui-btn ui-corner-all homeBtn'>Sign Up</a></div></div>");
 			}
     });	
 	
@@ -23,5 +23,5 @@ function logout() {
 	sessionStorage.clear();
 	$( "#menu" ).popup( "close" )
     
-	$("#userStatus").replaceWith("<div class='ui-grid-a' id='userStatus'><div class='ui-block-a'><a href='signin.html' data-ajax='false' class='ui-btn ui-corner-all'>Sign In</a></div><div class='ui-block-b'><a href='signup.html' class='ui-btn ui-corner-all'>Sign Up</a></div></div>");
+	$("#userStatus").replaceWith("<div class='ui-grid-a' id='userStatus'><div class='ui-block-a'><a href='signin.html' data-ajax='false' class='ui-btn ui-corner-all homeBtn'>Sign In</a></div><div class='ui-block-b'><a href='signup.html'  data-ajax='false' class='ui-btn ui-corner-all homeBtn'>Sign Up</a></div></div>");
 }
